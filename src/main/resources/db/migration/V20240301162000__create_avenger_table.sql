a6f665ff600259f213270b29efba1759a8df998d
Matheus Vieira <matheus.fvp@outlook.com>
2024-03-04 10:12:41 -0300

    fix - add missing comma to migration

--- src/main/resources/db/migration/V20240301162000__create_avenger_table.sql
+++ src/main/resources/db/migration/V20240301162000__create_avenger_table.sql
@@ -3,8 +3,8 @@ create table avenger (
                          nick varchar(36),
                          person varchar(128),
                          description varchar(128),
-                         history text
-                             primary key (id)
+                         history text,
+                         primary key (id)
 );
 
 alter table avenger add constraint UK_5r88eemotwgru6k0ilqb2ledh unique (nick);

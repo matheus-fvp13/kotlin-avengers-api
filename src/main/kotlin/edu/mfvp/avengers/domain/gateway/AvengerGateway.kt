c3562e7ee8a05c3c7f2aa38f5b7b61e7778f5401
Matheus Vieira <matheus.fvp@outlook.com>
2024-03-04 10:14:29 -0300

    created a gateway to access the domain layer

--- src/main/kotlin/edu/mfvp/avengers/domain/gateway/AvengerGateway.kt
+++ src/main/kotlin/edu/mfvp/avengers/domain/gateway/AvengerGateway.kt
@@ -1,10 +1,10 @@
-package edu.mfvp.avengers.domain.repositories
+package edu.mfvp.avengers.domain.gateway
 
 import edu.mfvp.avengers.domain.entities.Avenger
 import org.springframework.stereotype.Component
 
 @Component
-interface AvengerRepository {
+interface AvengerGateway {
     fun getDetail(id: Long): Avenger?
     fun getAvengers(): List<Avenger>
     fun create(avenger: Avenger): Avenger
